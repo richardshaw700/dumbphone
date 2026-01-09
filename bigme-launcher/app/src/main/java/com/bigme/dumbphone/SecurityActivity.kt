66ca731b45ad995c77d7b1e8206368a50b508e41
Richard Shaw <shaw.richard@icloud.com>
2026-01-09 05:22:07 -0500

    Security page: hide scrollbar, expand hardening details, fix tap-to-back

--- bigme-launcher/app/src/main/java/com/bigme/dumbphone/SecurityActivity.kt
+++ bigme-launcher/app/src/main/java/com/bigme/dumbphone/SecurityActivity.kt
@@ -1,13 +1,18 @@
 package com.bigme.dumbphone
 
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SecurityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_security)
+        
+        // Make the entire screen tappable to go back
+        findViewById<View>(R.id.securityRoot).setOnClickListener {
+            finish()
+        }
     }
     
     @Deprecated("Deprecated in Java")
